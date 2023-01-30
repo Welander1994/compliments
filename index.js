82ea97aeeaefc917aab3dd2ec69f4caffe170ec7
Jacob Welander <Jacobwelander1994@gmail.com>
2023-01-30 13:24:24 +0100

    link port in terminal

--- index.js
+++ index.js
@@ -78,5 +78,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log("Server running at http://localhost:3000");
 });
